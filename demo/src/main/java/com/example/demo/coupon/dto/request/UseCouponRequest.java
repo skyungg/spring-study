d0fdea1468382afeeb30b9af13bb3a1c34098759
허승경 <hhe98hsk@gmail.com>
2025-02-18 23:54:41 +0900

    feat: 쿠폰 사용 request 구현

---
+++ demo/src/main/java/com/example/demo/coupon/dto/request/UseCouponRequest.java
@@ -0,0 +1,25 @@
+package com.example.demo.coupon.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+@Getter
+@Builder
+@Validated
+@AllArgsConstructor
+public class UseCouponRequest {
+
+    @Schema(description = "쿠폰 사용을 원하는 사용자 아이디")
+    @NotNull
+    private Long userId;
+
+    @Schema(description = "현재 사용하려는 쿠폰 아이디")
+    @NotNull
+    private Long couponId;
+
+}
