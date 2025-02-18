7dea4c08786343303a296bd1a524e59a7b22de1c
허승경 <hhe98hsk@gmail.com>
2025-02-18 23:54:11 +0900

    feat: 쿠폰 service 구현

---
+++ demo/src/main/java/com/example/demo/coupon/service/CouponService.java
@@ -0,0 +1,48 @@
+package com.example.demo.coupon.service;
+
+import com.example.demo.coupon.domain.Coupon;
+import com.example.demo.coupon.dto.request.CouponValidationequest;
+import com.example.demo.coupon.dto.request.UseCouponRequest;
+import com.example.demo.coupon.repository.CouponRepository;
+import com.example.demo.coupon.validator.CouponValidator;
+import com.example.demo.exception.BadRequestException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.example.demo.exception.CustomExceptionStatus.*;
+
+@Service
+@RequiredArgsConstructor
+public class CouponService {
+    private final CouponRepository couponRepository;
+    private final CouponValidator couponValidator;
+    private final String INUSED_STATUS = "IN_USE";
+
+    @Transactional
+    public void useCoupon(final UseCouponRequest useCouponRequest) {
+        String status = couponRepository.findStatusById(useCouponRequest.getCouponId());
+
+        CouponValidationequest couponValidationequest =
+                CouponValidationequest.of(useCouponRequest,
+                        status);
+
+        couponValidator.validateCoupon(couponValidationequest);     // 쿠폰 검증
+
+        Coupon coupon =
+                couponRepository
+                        .findById(couponValidationequest.getCouponId())
+                        .orElseThrow(() -> new BadRequestException(NOT_EXISITED_COUPON_ID));
+
+        coupon.updateStatus(INUSED_STATUS);
+
+        couponRepository.save(coupon);
+
+//        try{
+//
+//
+//        }catch(Exception e){
+//            throw new BadRequestException(UNAVAILABLE_COUPON);
+//        }
+    }
+}
