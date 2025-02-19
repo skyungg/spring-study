d4d7eff82014d0e3212554433335d50caaafeced
허승경 <hhe98hsk@gmail.com>
2025-02-19 22:15:47 +0900

    feat: redis key 등록

--- demo/src/main/java/com/example/demo/coupon/service/CouponService.java
+++ demo/src/main/java/com/example/demo/coupon/service/CouponService.java
@@ -4,6 +4,7 @@ import com.example.demo.coupon.domain.Coupon;
 import com.example.demo.coupon.dto.request.CouponValidationequest;
 import com.example.demo.coupon.dto.request.UseCouponRequest;
 import com.example.demo.coupon.repository.CouponRepository;
+import com.example.demo.coupon.util.RedisUtil;
 import com.example.demo.coupon.validator.CouponValidator;
 import com.example.demo.exception.BadRequestException;
 import jakarta.transaction.Transactional;
@@ -18,6 +19,8 @@ public class CouponService {
     private final CouponRepository couponRepository;
     private final CouponValidator couponValidator;
     private final String INUSED_STATUS = "IN_USE";
+    private final RedisUtil redisUtil;
+    private final int TIMER = 120;
 
     @Transactional
     public void useCoupon(final UseCouponRequest useCouponRequest) {
@@ -35,8 +38,10 @@ public class CouponService {
                         .orElseThrow(() -> new BadRequestException(NOT_EXISITED_COUPON_ID));
 
         coupon.updateStatus(INUSED_STATUS);
-
         couponRepository.save(coupon);
+
+        String key = "COUPON_USER_" + useCouponRequest.getUserId();
+        redisUtil.setObjectExpire(key, useCouponRequest.getCouponId(), TIMER);      // 2분으로 설정
 
 //        try{
 //
