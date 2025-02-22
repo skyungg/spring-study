47e4924ecb98c0e70bb28f22cc23a5e87e0d038f
허승경 <hhe98hsk@gmail.com>
2025-02-22 15:33:25 +0900

    feat: 사용한 쿠폰 status 변경 로직 구현

--- demo/src/main/java/com/example/demo/coupon/service/CouponService.java
+++ demo/src/main/java/com/example/demo/coupon/service/CouponService.java
@@ -9,16 +9,21 @@ import com.example.demo.coupon.validator.CouponValidator;
 import com.example.demo.exception.BadRequestException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import static com.example.demo.exception.CustomExceptionStatus.*;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CouponService {
     private final CouponRepository couponRepository;
     private final CouponValidator couponValidator;
-    private final String INUSED_STATUS = "IN_USE";
+    private final String AVAILABLE_STATUS = "AVAILABLE";
+    private final String INUSE_STATUS = "IN_USE";
+    private final String APPLIED_STATUS = "APPLIED";
+    private final String EXPIRED_STATUS = "EXPIRED";
     private final RedisUtil redisUtil;
     private final int TIMER = 120;
 
@@ -37,17 +42,24 @@ public class CouponService {
                         .findById(couponValidationequest.getCouponId())
                         .orElseThrow(() -> new BadRequestException(NOT_EXISITED_COUPON_ID));
 
-        coupon.updateStatus(INUSED_STATUS);
+        log.info("[쿠폰 상태 변경 시도] 쿠폰 ID: {}, 현재 상태: {}", couponValidationequest.getCouponId(), coupon.getStatus());
+        coupon.updateStatus(INUSE_STATUS);
         couponRepository.save(coupon);
+        log.info("[쿠폰 상태 변경 완료] 쿠폰 ID: {}, 현재 상태: {}", couponValidationequest.getCouponId(), coupon.getStatus());
 
-        String key = "COUPON_USER_" + useCouponRequest.getUserId();
-        redisUtil.setObjectExpire(key, useCouponRequest.getCouponId(), TIMER);      // 2분으로 설정
+        String key = "USE_COUPON_" + couponValidationequest.getCouponId();
+        redisUtil.setObjectExpire(key, couponValidationequest.getUserId(), TIMER);      // 2분으로 설정
+        log.info("[쿠폰 REDIS 저장] key: {}, value: {}, TTL: {}초", key, couponValidationequest.getUserId(), TIMER);
+    }
 
-//        try{
-//
-//
-//        }catch(Exception e){
-//            throw new BadRequestException(UNAVAILABLE_COUPON);
-//        }
+    @Transactional
+    public void updateCouponStatusToExpired(Long couponId){
+        Coupon coupon = couponRepository.findById(couponId)
+                        .orElseThrow(() -> new BadRequestException(NOT_EXISITED_COUPON_ID));
+
+        log.info("[쿠폰 상태 변경 시도] 쿠폰 ID: {}, 현재 상태: {}", couponId, coupon.getStatus());
+        coupon.updateStatus(APPLIED_STATUS);
+        couponRepository.save(coupon);
+        log.info("[쿠폰 상태 변경 완료] 쿠폰 ID: {}, 현재 상태: {}", couponId, coupon.getStatus());
     }
 }
